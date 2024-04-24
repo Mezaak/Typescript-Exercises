1e4c7cd27325dc7ae249c31804aaf72440f10731
Mezaak <brnkky@gmail.com>
2024-04-24 21:57:18 +0300

    try

--- types.js
+++ types.js
@@ -1,2 +1,23 @@
-var number = 5;
-number = "a";
+var a = 5;
+var b = "a";
+var c = true;
+var d;
+var e = [1, 2, 3];
+var f = [1, 2, 3];
+var g = [1, "a", true];
+var h = ["a", 5, false]; //tuple
+var krediPayment = 0;
+var havalePayment = 1;
+var eftPayment = 2;
+//yukarı ile aşağının bir alakası yok
+var Payment;
+(function (Payment) {
+    Payment[Payment["kredi"] = 0] = "kredi";
+    Payment[Payment["havale"] = 1] = "havale";
+    Payment[Payment["kapidaodeme"] = 2] = "kapidaodeme";
+    Payment[Payment["eft"] = 3] = "eft";
+})(Payment || (Payment = {}));
+var kredi = Payment.kredi; //0
+var havale = Payment.havale; // 1
+var kapidaodeme = Payment.kapidaodeme; //2
+var eft = Payment.eft; // 3
