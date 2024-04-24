023b734a57b78e548109bd6c392e06016384a3b2
Mezaak <brnkky@gmail.com>
2024-04-24 23:43:30 +0300

    type-assertions

---
+++ type-assertions.js
@@ -0,0 +1,5 @@
+var message;
+message = "Hello World";
+var count = message.length;
+var lenges = message.length;
+//tsc type-assertions.ts
--- type-assertions.ts
+++ type-assertions.ts
@@ -4,3 +4,6 @@ message = "Hello World";
 
 let count = (<string>message).length;
 let lenges = (message as string).length;
+
+//tsc type-assertions.ts
+
