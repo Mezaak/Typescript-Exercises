c1f1963c3d3215a96a211d1c9f57a2680414a916
Mezaak <brnkky@gmail.com>
2024-04-25 16:46:39 +0300

    asd

--- interface.ts
+++ interface.ts
@@ -1,13 +1,13 @@
-let travelTo = (point:{x:number,y: number}) => {
-    //..
-}
+// let travelTo = (point:{x:number,y: number}) => {
+//     //..
+// }
 
-let getDistance = (pointA:{x:number,y: number},pointB:{x:number,y: number});
+// let getDistance = (pointA:{x:number,y: number},pointB:{x:number,y: number});
 
-travelTo({
-    x:1,
-    y:2
-});
+// travelTo({
+//     x:1,
+//     y:2
+// });
 
 
 
