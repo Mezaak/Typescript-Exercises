ab83bec2ce2f97cddd80087d966df96edb9a21f4
Mezaak <brnkky@gmail.com>
2024-04-25 17:17:37 +0300

    asdasd

---
+++ class.ts
@@ -0,0 +1,17 @@
+interface Point {
+    x:number,
+    y:number
+}
+
+interface Passenger{
+    name:string;
+    phone:string;
+}
+
+interface Vehicle{
+    currentLocation: Point;
+    travelTo(point:Point):void;
+    getDistance(pointA:Point,pointB:Point):number;
+    addPassengers(passenger:Passenger):void;
+    removePassenger(passenger:Passenger):void;
+}
