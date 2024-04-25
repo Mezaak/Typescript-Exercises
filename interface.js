0d559bd6eb84f3355e94b210e2737786779a6b19
Mezaak <brnkky@gmail.com>
2024-04-25 16:53:27 +0300

    int js

---
+++ interface.js
@@ -0,0 +1,13 @@
+// let travelTo = (point:{x:number,y: number}) => {
+//     //..
+// }
+var travelTo = function (point) {
+    //..
+};
+var getDistance = function (pointA, pointB) {
+    //..
+};
+travelTo({
+    x: 1,
+    y: 2
+});
